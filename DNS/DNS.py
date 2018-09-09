074ef51fe47c4dc8e8309bfb5e363881613a8798
Luiz Mattos <luizgustavomattoswow@gmail.com>
2018-09-09 11:08:36 -0300

    Atualização do DNS

--- DNS/DNS.py
+++ DNS/DNS.py
@@ -6,42 +6,54 @@ import time
 HOST = ''              # Endereco IP do Servidor
 PORT = 9996            # Porta que o Servidor esta
 
-def conectado(con, cliente):
-	print ("Cliente conectado server DNS")
-	
-	msgSerializada = con.recv(1024)
-	msg = pickle.loads(msgSerializada)
-	
-	print ("MENSAGEM RECEBIDA DO CLIENTE: ",msg)
+class DNS:
+	def __init__(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		orig = (HOST, PORT)
+		self.s.bind(orig)
+		self.s.listen(1)
+		self.lista = list()
 		
-	if msg == 'mySQL':
-		msg = ('127.0.0.1',9997)
-	elif msg == 'HTTP':
-		msg = ('127.0.0.1',9998)
-	elif msg == 'CHAT':
-		msg = ('127.0.0.1',9999)
-	else: msg = ('127.0.0.1',9996)
-	
-	
-	msgSerializada = pickle.dumps(msg)
-	
-	print ("MSG QUE SERA ENVIADA: ",msg)
-	
-	con.send(msgSerializada)
-	
-	
-	con.close()
-	
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#Cria lista de servers		
+		self.lista.append(['mySQL','127.0.0.1',9997])
+		self.lista.append(['HTTP','127.0.0.1',9998])
+		self.lista.append(['CHAT','127.0.0.1',9999])
+		self.lista.append(['DNS','127.0.0.1',9996])
+				
+		self.listen()
 
-orig = (HOST, PORT)
- 
-s.bind(orig)
-s.listen(1)
-
-while True:
-	con, cliente = s.accept()
-	t = threading.Thread(target=conectado ,args=(con, cliente))
-	t.start()
-
-s.close()
+	def getAddress(self, host):
+		for i in self.lista:
+			name, address, port = i
+			if name == host: return address, port
+			
+		return -1
+		
+	def listen(self):
+		while True:
+			con, cliente = self.s.accept()
+			t = threading.Thread(target = self.clienteConectado ,args=(con, cliente))
+			t.start()
+	
+	def clienteConectado(self,con, cliente):
+		print ("Cliente conectado server DNS")
+		
+		msgSerializada = con.recv(1024)
+		msg = pickle.loads(msgSerializada)
+		
+		print ("MENSAGEM RECEBIDA DO CLIENTE: ",msg)
+			
+		host = self.getAddress(msg)
+		
+		msgSerializada = pickle.dumps(host)
+		
+		print ("MSG QUE SERA ENVIADA: ",host)
+		
+		con.send(msgSerializada)
+		
+		con.close()
+		
+	def exit(self):
+		self.s.close()
+	
+dns = DNS()
